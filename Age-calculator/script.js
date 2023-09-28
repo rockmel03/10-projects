87ebe27109b263c6ef4474a885dfe4943729db5e
rockmel03 <kamalswag2@gmail.com>
2023-09-28 21:17:20 +0530

    making 10th project

---
+++ Age-calculator/script.js
@@ -0,0 +1,30 @@
+const inputAge = document.querySelector('#input');
+const formEl = document.querySelector('.container')
+const result = document.querySelector('#result')
+
+function calculateAge(value) {
+
+    let currentYear = new Date().getFullYear()
+    let userYear = new Date(value).getFullYear();
+    let age = currentYear - userYear
+
+    let currentMonth = new Date().getMonth()
+    let userMonth = new Date(value).getMonth()
+
+    if (currentMonth < userMonth) {
+        age--
+    }
+    return age
+}
+
+
+formEl.addEventListener('submit', function (e) {
+    e.preventDefault()
+    let Age = calculateAge(inputAge.value)
+    console.log(Age)
+    if (Age >= 0) {
+        result.innerHTML = `Your Age is ${Age} Years Old`
+    } else {
+        result.innerHTML = `please enter a valid age`
+    }
+})
